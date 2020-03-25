9378c738e7d12afa62a6032ed1231ae09bc6eeff
aliciaberger <aberger@fwparker.org>
2020-03-25 13:10:52 -0500

    Merge branch 'master' of https://github.com/fwparkercode/P2_SP20
    # Conflicts:
    #	Problems/Sorting/05_sorting_problems.py

--- Labs/CTA Ridership/cta_rider_lab.py
+++ Labs/CTA Ridership/cta_rider_lab.py
@@ -5,20 +5,60 @@ Get the csv from the following data set.
 https://data.cityofchicago.org/api/views/w8km-9pzd/rows.csv?accessType=DOWNLOAD
 This shows CTA ridership by year going back to the 80s
 It has been updated with 2018 data, but not yet with 2019 unfortunately
-
-
-1  Make a line plot of rail usage for the last 10 years of data.  (year on x axis, and ridership on y) (5pts)
-2  Plot bus usage for the same years as a second line on your graph. (5pts)
-3  Plot total usage on a third line on your graph. (5pts)
-4  Add a title and label your axes. (4pts)
-5  Add a legend to show data represented by each of the three lines. (4pts)
-6  What trend or trends do you see in the data?  Offer a hypotheses which might explain the trend(s). Just add a comment here to explain. (2pts)
 '''
 
 import csv
+import matplotlib.pyplot as plt
 
 with open("CTA_-_Ridership_-_Annual_Boarding_Totals (1).csv") as f:
     cr = csv.reader(f)
     data = list(cr)
+print(len(data))
+headers = data.pop(0)
+print(headers)
 
+#1  Make a line plot of rail usage for the last 10 years of data.  (year on x axis, and ridership on y) (5pts)
+data.sort(reverse=True, key=lambda x: int(x[0]))  # sorts by whatever is in there
 print(data)
+
+years = [x[0] for x in data]
+top_ten = years[0:10]
+print(top_ten)
+
+rail = [int(x[-2]) for x in data]
+rail_ten = rail[0:10]
+print(rail_ten)
+
+
+#2  Plot bus usage for the same years as a second line on your graph. (5pts)
+
+bus = [int(x[1]) for x in data]
+bus_ten = bus[0:10]
+print(bus_ten)
+
+
+
+#3  Plot total usage on a third line on your graph. (5pts)
+total = [int(x[-1]) for x in data]
+total_ten = total[0:10]
+print(total_ten)
+
+
+plt.title("Last ten years transit")
+plt.ylabel('Number of Rides')
+plt.xlabel('Years')
+plt.plot(top_ten, total_ten, color='red', label='bus')
+plt.plot(top_ten, rail_ten, color='darkgreen', label='rail')
+plt.plot(top_ten, bus_ten, color='blue', label='bus')
+plt.show()
+#4  Add a title and label your axes. (4pts)
+
+
+
+#5  Add a legend to show data represented by each of the three lines. (4pts)
+
+
+#6  What trend or trends do you see in the data?  Offer a hypotheses which might explain the trend(s). Just add a comment here to explain. (2pts)
+
+
+
--- Notes/NotesB/yes_10_plotting_csv.py
+++ Notes/NotesB/yes_10_plotting_csv.py
@@ -10,39 +10,42 @@ print(len(data))
 headers = data.pop(0)
 print(headers)
 
-data.sort(key=lambda x: int(x[-1]))
+data.sort(key=lambda x: int(x[-1]))  # sorts by whatever is in there
 print(data)
 
-library_names = [x[0] for x in data]
+library_names = [x[0] for x in data]    # is list of what ever subset is in []
 print(library_names)
 
-monthly_data = [x[4:-1]for x in data]
-print(monthly_data)
+#monthly_data = [x[4:-1]for x in data]
+#print(monthly_data)
 
-my_library = monthly_data[library_names.index('Lincoln Park')]
-my_library = [int(x) for x in my_library]
-print(my_library)
+#my_library = monthly_data[library_names.index('Lincoln Park')]
+#my_library = [int(x) for x in my_library]
+#print(my_library)
 
+
+
+'''
 library2 = monthly_data[library_names.index('Bucktown-Wicker Park')]
 library2 = [int(x) for x in library2]
-
+'''
 
 plt.figure(1, tight_layout=True)
-month_numbers = [x for x in range(12)]
-month_names = headers[4:-1]
-print(month_names)
+#month_numbers = [x for x in range(12)]     #have to set up value
+#month_names = headers[4:-1]  # give the names [:] not including last
+#print(month_names)
 
-plt.plot(month_numbers, my_library, color='darkgreen', label='lincon park')
-plt.plot(month_numbers, library2, color='darkblue', label='Wikcerpark')
+#plt.plot(month_numbers, my_library, color='darkgreen', label='lincon park')
+# plt.plot(month_numbers, library2, color='darkblue', label='Wikcerpark')
 
 #plt.bar(month_numbers, my_library, color='lightblue', edgecolor='black')
-plt.xticks(month_numbers, month_names, rotation=45)  # replaces the shown values on the graph axis
+#plt.xticks(month_numbers, month_names, rotation=45)  # replaces the shown values on the graph axis
 # axis
 
-plt.axis([0, 11, 0, 16000])
-plt.title("Lincon park Library")
-plt.ylabel('Visitors')
-plt.legend()
+#plt.axis([0, 11, 0, 16000])
+#plt.title("Lincon park Library")
+#plt.ylabel('Visitors')
+#plt.legend()
 
 
 
@@ -50,9 +53,9 @@ plt.legend()
 plt.figure(2, tight_layout =True, figsize=(8,8))
 #plot every library in chi YTD Visitors
 
-ytd_list = [int(x[-1]) for x in data]
+ytd_list = [int(x[-1]) for x in data]  # makes a list of things is colomb []
 print(ytd_list)
-library_numbers = [x for x in range(len(library_names))]
+library_numbers = [x for x in range(len(library_names))]   # how many things you have
 
 plt.barh(library_numbers, ytd_list)
 plt.yticks(library_numbers, library_names, fontsize=6)
