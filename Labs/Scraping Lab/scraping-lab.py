e9d6c925b163fafbfa86ff05c97e0437e0cf540a
aliciaberger <aberger@fwparker.org>
2020-05-04 09:26:05 -0500

    Merge branch 'master' of https://github.com/fwparkercode/P2_SP20
    # Conflicts:
    #	Notes/NotesB/style.css

--- Labs/Scraping Lab/scraping-lab.py
+++ Labs/Scraping Lab/scraping-lab.py
@@ -1,16 +1,4 @@
 # SCRAPING PROBLEMS
-# Twitter Scraping (15pts)
-# Go to your favorite follow on Twitter.  (not someone who posts explicit materials please)
-# Inspect the twitter feed in Chrome.
-# You'll notice that the tweets are stored in a ordered list <ol></ol>, and individual tweets are contained as list items <li></li>.
-# Use BeautifulSoup and requests to grab the text contents of last 5 tweetslocated on the twitter page you chose.
-# Print the tweets in a nicely formatted way.
-# Have fun.  Again, nothing explicit.
-
-#      print("{} {}!".format("Hello", "World"))
-
-
-
 
 # Weather Scraping (15pts)
 # Below is a link to a 10-day weather forecast at weather.com
@@ -25,5 +13,65 @@
 # Wednesday, April 4 will be Partly Cloudy/Windy with a High of 37 degrees and a low of 25, humidity at 52%.  There is 0% chance of rain with winds out of the WNW at 22 mph.
 # if the sentence is a little different than shown, that will work; do what you can.  Don't forget about our friend string.format()
 
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://weather.com/weather/tenday/l/0df6c5abacc433681eec19026bb8b030e3ccf3b69830725cb987ada967f618cf" #uniform resource locator
+
+page = requests.get(url)
+#print(page)  #prints the response
+
+print(page.text)
+
+soup = BeautifulSoup(page.text, "html.parser")
+print(soup.prettify()) #nicer way to look at it
+
+#find some data by the tag name
+#find method
+title = soup.find("title") #title is  just the  tag name
+print(title)
+print(title.text)
 
 
+#by attribute
+
+print("\n" * 10)
+days = soup.find_all(class_="day-detail clearfix")
+print(days)
+print(type(days))
+
+synopsis = soup.find(class_="")
+print(days)
+print(type(days))
+
+
+for day in days:
+    print(day.text)
+    print(synopsis.text)
+    print()
+
+#by css class
+'''
+print("\n" * 10)
+quotes = soup.find_all(class_="quote")
+print(quotes[0].prettify())
+
+# drill down using dot notation
+
+print(quotes[0].span)
+print(quotes[0].span.text)
+
+for quote in quotes:
+    print(quote.span.text)
+
+for i in range(len(quotes)):
+    print(quotes[i])
+    print("\t\t---{}".format(authors[i].text))
+    print()
+
+quotes = soup.find_all("span", class_="text", itemprop="text")
+
+for quote in quotes:
+    print(quote.text)
+
+'''
